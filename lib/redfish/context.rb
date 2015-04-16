33e9cb656572ddd1fe1ebd0aebf5a1e2a77824bf
Peter Donald <peter@realityforge.org>
2015-04-16 13:58:47 +1000

    Add some more values to context

--- lib/redfish/context.rb
+++ lib/redfish/context.rb
@@ -11,9 +11,28 @@ module Redfish
     # The password file used when connecting to glassfish.
     attr_reader :domain_password_file
 
-    def initialize(domain_name, domain_admin_port, domain_secure, domain_username, domain_password_file)
+    # Use terse output from the underlying asadmin.
+    def terse?
+      !!@terse
+    end
+
+    #If true, echo commands supplied to asadmin.
+    def echo?
+      !!@echo
+    end
+
+    # The user that the domain executes as.
+    attr_reader :system_user
+    # The group that the domain executes as.
+    attr_reader :system_group
+
+    def initialize(domain_name, domain_admin_port, domain_secure, domain_username, domain_password_file, options = {})
       @domain_name, @domain_admin_port, @domain_secure, @domain_username, @domain_password_file =
         domain_name, domain_admin_port, domain_secure, domain_username, domain_password_file
+      @echo = options[:echo].nil? ? false : !!options[:echo]
+      @terse = options[:terse].nil? ? false : !!options[:terse]
+      @system_user = options[:system_user]
+      @system_group = options[:system_group]
       @property_cache = nil
     end
 
--- test/test_context.rb
+++ test/test_context.rb
@@ -8,14 +8,29 @@ class TestContext < Redfish::TestCase
     domain_secure = true
     domain_username = 'admin'
     domain_password_file = '/etc/glassfish/password'
+    system_user = 'glassfish'
+    system_group = 'glassfish_group'
 
-    context = Redfish::Context.new(domain_name, domain_admin_port, domain_secure, domain_username, domain_password_file)
+    context = Redfish::Context.new(domain_name,
+                                   domain_admin_port,
+                                   domain_secure,
+                                   domain_username,
+                                   domain_password_file,
+                                   :system_user => system_user,
+                                   :system_group => system_group,
+                                   :terse => true,
+                                   :echo => true)
 
     assert_equal context.domain_name, domain_name
     assert_equal context.domain_admin_port, domain_admin_port
     assert_equal context.domain_secure, domain_secure
     assert_equal context.domain_username, domain_username
     assert_equal context.domain_password_file, domain_password_file
+
+    assert_equal context.terse?, true
+    assert_equal context.echo?, true
+    assert_equal context.system_user, system_user
+    assert_equal context.system_group, system_group
 
     assert !context.property_cache?
 
