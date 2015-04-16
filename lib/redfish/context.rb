ed8135cd0cd0c602e43fe1a1b91dec4d977e274b
Peter Donald <peter@realityforge.org>
2015-04-16 13:58:30 +1000

    Add documentation

--- lib/redfish/context.rb
+++ lib/redfish/context.rb
@@ -1,9 +1,14 @@
 module Redfish
   class Context
+    # The name of the domain.
     attr_reader :domain_name
+    # The port on which the management application is bound.
     attr_reader :domain_admin_port
+    # If true use SSL when communicating with the domain for administration. Assumes the domain is in secure mode.
     attr_reader :domain_secure
+    # The username to use when communicating with the domain.
     attr_reader :domain_username
+    # The password file used when connecting to glassfish.
     attr_reader :domain_password_file
 
     def initialize(domain_name, domain_admin_port, domain_secure, domain_username, domain_password_file)
