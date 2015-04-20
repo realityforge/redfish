0da26591064ec76ddc3690fd56e30f7867319981
Peter Donald <peter@realityforge.org>
2015-04-20 10:38:54 +1000

    Add further tests

--- test/tasks/test_jdbc_connection_pool.rb
+++ test/tasks/test_jdbc_connection_pool.rb
@@ -151,7 +151,7 @@ class Redfish::Tasks::TestProperty < Redfish::TestCase
       returns("APool\n")
 
     {
-      'description' => 'Audit Connection Pool',
+      'description' => 'X',
       'datasource-classname' => 'net.sourceforge.jtds.jdbcx.JtdsDataSource',
       'init-sql' => '',
       'sql-trace-listeners' => '',
@@ -213,6 +213,10 @@ class Redfish::Tasks::TestProperty < Redfish::TestCase
                                  equals(:terse => true, :echo => false))
     executor.expects(:exec).with(equals(t.context),
                                  equals('set'),
+                                 equals(['resources.jdbc-connection-pool.APool.description=Audit Connection Pool']),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
                                  equals(['resources.jdbc-connection-pool.APool.ping=true']),
                                  equals(:terse => true, :echo => false))
 
@@ -255,6 +259,155 @@ class Redfish::Tasks::TestProperty < Redfish::TestCase
     t.perform_action(:create)
 
     assert_equal t.updated_by_last_action?, true
+    ensure_expected_cache_values(t)
+  end
+
+  def test_cache_and_present_but_modified
+    cache_values = get_expected_cache_values
+
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    cache_values['resources.jdbc-connection-pool.APool.ping'] = 'false'
+    cache_values['resources.jdbc-connection-pool.APool.description'] = 'XXX'
+    cache_values['resources.jdbc-connection-pool.APool.deployment-order'] = '101'
+    cache_values['resources.jdbc-connection-pool.APool.property.Password'] = 'secret'
+
+    t.context.cache_properties(cache_values)
+
+    t.options = {'pool_name' => 'APool',
+                 'datasourceclassname' => 'net.sourceforge.jtds.jdbcx.JtdsDataSource',
+                 'restype' => 'javax.sql.DataSource',
+                 'validationmethod' => 'auto-commit',
+                 'isconnectvalidatereq' => 'true',
+                 'ping' => 'true',
+                 'description' => 'Audit Connection Pool',
+                 'deploymentorder' => 100,
+                 'properties' =>
+                   {
+                     'Instance' => 'MSSQLSERVER',
+                     'ServerName' => 'db.example.com',
+                     'User' => 'sa',
+                     'Password' => 'password',
+                     'PortNumber' => '1234',
+                     'DatabaseName' => 'MYDB'
+                   }}
+
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(['resources.jdbc-connection-pool.APool.property.Password=password']),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(['resources.jdbc-connection-pool.APool.deployment-order=100']),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(['resources.jdbc-connection-pool.APool.description=Audit Connection Pool']),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(['resources.jdbc-connection-pool.APool.ping=true']),
+                                 equals(:terse => true, :echo => false))
+    t.perform_action(:create)
+
+    assert_equal t.updated_by_last_action?, true
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_cache_and_present
+    cache_values = get_expected_cache_values
+
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties(cache_values)
+
+    t.options = {'pool_name' => 'APool',
+                 'datasourceclassname' => 'net.sourceforge.jtds.jdbcx.JtdsDataSource',
+                 'restype' => 'javax.sql.DataSource',
+                 'validationmethod' => 'auto-commit',
+                 'isconnectvalidatereq' => 'true',
+                 'ping' => 'true',
+                 'description' => 'Audit Connection Pool',
+                 'deploymentorder' => 100,
+                 'properties' =>
+                   {
+                     'Instance' => 'MSSQLSERVER',
+                     'ServerName' => 'db.example.com',
+                     'User' => 'sa',
+                     'Password' => 'password',
+                     'PortNumber' => '1234',
+                     'DatabaseName' => 'MYDB'
+                   }}
+
+    t.perform_action(:create)
+
+    assert_equal t.updated_by_last_action?, false
+
+    ensure_expected_cache_values(t)
+  end
+
+  def ensure_expected_cache_values(t)
+    get_expected_cache_values.each_pair do |key, value|
+      assert_equal t.context.property_cache[key], value, "Expected #{key}=#{value}"
+    end
+  end
+
+  def get_expected_cache_values
+    cache_values = {}
+
+    {
+      'description' => 'Audit Connection Pool',
+      'datasource-classname' => 'net.sourceforge.jtds.jdbcx.JtdsDataSource',
+      'init-sql' => '',
+      'sql-trace-listeners' => '',
+      'driver-classname' => '',
+      'validation-classname' => '',
+      'validation-table-name' => '',
+      'steady-pool-size' => '8',
+      'max-pool-size' => '32',
+      'max-wait-time-in-millis' => '60000',
+      'pool-resize-quantity' => '2',
+      'idle-timeout-in-seconds' => '300',
+      'validate-atmost-once-period-in-seconds' => '0',
+      'connection-leak-timeout-in-seconds' => '0',
+      'statement-leak-timeout-in-seconds' => '0',
+      'connection-creation-retry-attempts' => '0',
+      'connection-creation-retry-interval-in-seconds' => '10',
+      'statement-timeout-in-seconds' => '-1',
+      'max-connection-usage-count' => '0',
+      'statement-cache-size' => '0',
+      'is-isolation-level-guaranteed' => 'true',
+      'is-connection-validation-required' => 'true',
+      'fail-all-connections' => 'false',
+      'allow-non-component-callers' => 'false',
+      'non-transactional-connections' => 'false',
+      'statement-leak-reclaim' => 'false',
+      'connection-leak-reclaim' => 'false',
+      'lazy-connection-enlistment' => 'false',
+      'lazy-connection-association' => 'false',
+      'associate-with-thread' => 'false',
+      'match-connections' => 'false',
+      'ping' => 'true',
+      'pooling' => 'true',
+      'wrap-jdbc-objects' => 'true',
+      'res-type' => 'javax.sql.DataSource',
+      'transaction-isolation-level' => '',
+      'connection-validation-method' => 'auto-commit',
+      'property.Instance' => 'MSSQLSERVER',
+      'property.ServerName' => 'db.example.com',
+      'property.User' => 'sa',
+      'property.Password' => 'password',
+      'property.PortNumber' => '1234',
+      'property.DatabaseName' => 'MYDB',
+      'deployment-order' => '100'
+    }.each_pair do |k, v|
+      cache_values["resources.jdbc-connection-pool.APool.#{k}"] = "#{v}"
+    end
+    cache_values
   end
 
   def new_task(executor)
