76e37622cbc02e3a0816d34a22de94af4af163f9
Peter Donald <peter@realityforge.org>
2015-04-20 13:41:34 +1000

    Write a collect_property_sets that used to collect sets and unsets on properties within glassfish resources

--- lib/redfish/tasks/asadmin_task.rb
+++ lib/redfish/tasks/asadmin_task.rb
@@ -6,6 +6,37 @@ module Redfish
 
       protected
 
+      #
+      # Many glassfish resources have an "extensible" set of properties with declared under the resource.
+      # This method assumes there is a method "properties" that returns a list of said properties. It then
+      # adds each these properties to a map with name relative to the resource. It also adds empty properties
+      # to the map for any properties that need to be removed.
+      #
+      # e.g.
+      #    collect_property_sets('resources.jdbc-connection-pool.MyDbPool.', property_map)
+      #
+      def collect_property_sets(property_prefix, property_map)
+        self.properties.each_pair do |key, value|
+          property_map["property.#{key}"] = as_property_value(value)
+        end
+
+        full_prefix = "#{property_prefix}property."
+
+        extra_property_keys =
+          context.property_cache? ?
+            context.property_cache.get_keys_starting_with(full_prefix) :
+            load_properties("#{full_prefix}*").keys
+
+        self.properties.keys.each do |k|
+          extra_property_keys.delete("#{full_prefix}#{k}")
+        end
+
+        extra_property_keys.each do |key|
+          k = key[full_prefix.length..-1]
+          property_map["property.#{k}"] = ''
+        end
+      end
+
       def load_properties(pattern)
         output = context.exec('get', [pattern], :terse => true, :echo => false)
         parse_properties(output)
--- test/tasks/test_asadmin_task.rb
+++ test/tasks/test_asadmin_task.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../../helper', __FILE__)
 
 class Redfish::Tasks::TestAsadminTask < Redfish::TestCase
+  class MyAsadminTask < Redfish::Tasks::AsadminTask
+    attribute :properties, :kind_of => Hash, :default => {}
+  end
+
   def test_load_properties
     executor = Redfish::Executor.new
     t = new_task(executor)
@@ -28,8 +32,47 @@ class Redfish::Tasks::TestAsadminTask < Redfish::TestCase
     assert_equal properties['c.d.e'], '345'
   end
 
+  def collect_property_sets_with_cache_present
+    property_prefix = 'resources.jdbc-connection-pool.MyDbPool.'
+
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    cache_values = {}
+    cache_values["#{property_prefix}property.c"] = '3'
+
+    t.context.cache_properties(cache_values)
+
+    t.properties = {'a' => '1', 'b' => '2'}
+
+    property_map = []
+
+    t.send(:collect_property_sets, property_prefix, property_map)
+
+    assert_equal property_map, {'property.a' => '1', 'property.b' => '2', 'property.c' => ''}
+  end
+
+  def collect_property_sets_with_no_cache_present
+    property_prefix = 'resources.jdbc-connection-pool.MyDbPool.'
+
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    # Return a property that should be deleted
+    executor.expects(:exec).with(equals(t.context), equals('get'), equals(%w(resources.jdbc-connection-pool.APool.property.*)), equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}property.c=3\n")
+
+    t.properties = {'a' => '1', 'b' => '2'}
+
+    property_map = []
+
+    t.send(:collect_property_sets, property_prefix, property_map)
+
+    assert_equal property_map, {'property.a' => '1', 'property.b' => '2', 'property.c' => ''}
+  end
+
   def new_task(executor)
-    t = Redfish::Tasks::AsadminTask.new
+    t = MyAsadminTask.new
     t.context = Redfish::Context.new(executor, '/opt/payara-4.1.151/', 'domain1', 4848, false, 'admin', nil)
     t
   end
