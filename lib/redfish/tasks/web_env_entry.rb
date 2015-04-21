d4175cabef4d5d17f0e4b48f44ec2c0e5ec11f88
Peter Donald <peter@realityforge.org>
2015-04-21 20:46:51 +1000

    Add web_env_entry task

---
+++ lib/redfish/tasks/web_env_entry.rb
@@ -0,0 +1,79 @@
+module Redfish
+  module Tasks
+    class WebEnvEntry < BaseResourceTask
+
+      # The name of the associated application.
+      attribute :application, :kind_of => String, :required => true
+      # The optional name of the module if part of multiple module application.
+      attribute :module, :kind_of => String, :default => nil
+      # The key name of the web env entry.
+      attribute :name, :kind_of => String, :required => true
+      # The java type name of env entry.
+      attribute :type, :equal_to => %w(java.lang.Boolean java.lang.Byte java.lang.Character java.lang.Double java.lang.Float java.lang.Integer java.lang.Long java.lang.Short java.lang.String), :default => 'java.lang.String'
+      # The value of the entry.
+      attribute :value, :kind_of => String, :default => nil
+      # Specifies whether the environment entry is ignored if it is set in the application's deployment descriptor. When an environment entry is ignored, the application behaves as if the entry had never been set in the application's deployment descriptor.
+      attribute :ignore_descriptor_item, :equal_to => [true, false, 'true', 'false'], :default => false
+      # A description of the entry.
+      attribute :description, :kind_of => String, :default => nil
+
+      action :create do
+        raise 'At least one of the "ignore_descriptor_item" or "value" parameters must be set.' if !self.ignore_descriptor_item? && self.value.nil?
+        raise 'Only one of "ignore_descriptor_item" or "value" parameters must be set but both are specified.' if self.ignore_descriptor_item? && !self.value.nil?
+        create(resource_property_prefix)
+      end
+
+      action :destroy do
+        destroy(resource_property_prefix)
+      end
+
+      def ignore_descriptor_item?
+        self.ignore_descriptor_item.to_s == 'true'
+      end
+
+      def resource_property_prefix
+        "applications.application.#{self.application}.module.#{self.module.nil? ? self.application : self.module}.engine.web.web-module-config.env-entry.#{self.name}."
+      end
+
+      def properties_to_record_in_create
+        {'env-entry-name' => self.name}
+      end
+
+      def properties_to_set_in_create
+        property_map = {}
+
+        property_map['description'] = self.description
+        property_map['env-entry-type'] = self.ignore_descriptor_item? ? '' : self.type
+        property_map['env-entry-value'] = self.ignore_descriptor_item? ? '' : self.value
+        property_map['ignore-descriptor-item'] = self.ignore_descriptor_item
+
+        property_map
+      end
+
+      def do_create
+        args = []
+
+        args << '--name' << self.name.to_s
+        args << '--type' << self.type.to_s unless self.ignore_descriptor_item?
+        args << '--description' << self.description.to_s
+        args << '--value' << self.value.to_s unless self.value.nil?
+        args << "--ignoreDescriptorItem=#{self.ignore_descriptor_item}"
+        args << application_spec
+
+        context.exec('set-web-env-entry', args)
+      end
+
+      def application_spec
+        "#{self.application.to_s}#{self.module.nil? ? '' : "/#{self.module}"}"
+      end
+
+      def do_destroy
+        context.exec('unset-web-env-entry', ['--name', self.name, application_spec])
+      end
+
+      def present?
+        (context.exec('list-web-env-entry', [application_spec], :terse => true, :echo => false) =~ /^#{Regexp.escape(self.name)} (#{Regexp.escape('(java.lang.')}|#{Regexp.escape('(null)')})/)
+      end
+    end
+  end
+end
---
+++ test/tasks/test_web_env_entry.rb
@@ -0,0 +1,236 @@
+require File.expand_path('../../helper', __FILE__)
+
+class Redfish::Tasks::TestWebEnvEntry < Redfish::Tasks::BaseTaskTest
+  def test_create_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-web-env-entry'), equals(%w(MyApp)), equals(:terse => true, :echo => false)).
+      returns('')
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set-web-env-entry'),
+                                 equals(['--name', 'MyEntry', '--type', 'java.lang.String', '--description', 'My Entry Desc', '--value', 'Blah', '--ignoreDescriptorItem=false', 'MyApp']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-web-env-entry'), equals(%w(MyApp)), equals(:terse => true, :echo => false)).
+      returns("MyEntry (java.lang.String) = x ignoreDescriptorItem=true //(description not specified)\n")
+
+    expected_local_properties.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present_but_modified
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-web-env-entry'), equals(%w(MyApp)), equals(:terse => true, :echo => false)).
+      returns("MyEntry (null) = null ignoreDescriptorItem=false //(description not specified)\n")
+
+    values = expected_local_properties
+    values['env-entry-value'] = 'NotTheBaby'
+
+    values.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}env-entry-value=Blah"]),
+                                 equals(:terse => true, :echo => false)).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties({})
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set-web-env-entry'),
+                                 equals(['--name', 'MyEntry', '--type', 'java.lang.String', '--description', 'My Entry Desc', '--value', 'Blah', '--ignoreDescriptorItem=false', 'MyApp']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present_but_modified
+    cache_values = expected_properties
+
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    cache_values["#{property_prefix}env-entry-value"] = 'X'
+    cache_values["#{property_prefix}description"] = 'XXX'
+
+    t.context.cache_properties(cache_values)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}env-entry-value=Blah"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}description=My Entry Desc"]),
+                                 equals(:terse => true, :echo => false))
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present
+    t = new_task
+
+    t.context.cache_properties(expected_properties)
+
+    t.options = resource_parameters
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'application' => 'MyApp', 'name' => 'MyEntry'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-web-env-entry'),
+                                 equals(['MyApp']),
+                                 equals({:terse => true, :echo => false})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'application' => 'MyApp', 'name' => 'MyEntry'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-web-env-entry'),
+                                 equals(['MyApp']),
+                                 equals({:terse => true, :echo => false})).
+      returns("MyEntry (null)\n")
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('unset-web-env-entry'),
+                                 equals(['--name', 'MyEntry', 'MyApp']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_not_present
+    t = new_task
+
+    t.context.cache_properties({})
+    t.options = {'application' => 'MyApp', 'name' => 'MyEntry'}
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties(expected_properties)
+    t.options = {'application' => 'MyApp', 'name' => 'MyEntry'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('unset-web-env-entry'),
+                                 equals(['--name', 'MyEntry', 'MyApp']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  protected
+
+  def property_prefix
+    'applications.application.MyApp.module.MyApp.engine.web.web-module-config.env-entry.MyEntry.'
+  end
+
+  # Properties in GlassFish properties directory
+  def expected_local_properties
+    {
+      'ignore-descriptor-item' => 'false',
+      'env-entry-type' => 'java.lang.String',
+      'env-entry-value' => 'Blah',
+      'description' => 'My Entry Desc'
+    }
+  end
+
+  # Resource parameters
+  def resource_parameters
+    {
+      'name' => 'MyEntry',
+      'application' => 'MyApp',
+      'type' => 'java.lang.String',
+      'value' => 'Blah',
+      'description' => 'My Entry Desc'
+    }
+  end
+end
