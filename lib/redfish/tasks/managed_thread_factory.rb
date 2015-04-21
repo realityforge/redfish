0b0ae2a456a4f12b2ada2bc93104b80f82a156d2
Peter Donald <peter@realityforge.org>
2015-04-22 09:04:41 +1000

    Implement ManagedThreadFactory resource

---
+++ lib/redfish/tasks/managed_thread_factory.rb
@@ -0,0 +1,73 @@
+module Redfish
+  module Tasks
+    class ManagedThreadFactory < BaseResourceTask
+
+      attribute :name, :kind_of => String, :required => true
+
+      # Determines whether the resource is enabled at runtime.
+      attribute :enabled, :equal_to => [true, false, 'true', 'false'], :default => true
+      # Determines whether container contexts are propagated to threads. If set to true, the contexts specified in the --contextinfo option are propagated. If set to false, no contexts are propagated and the --contextinfo option is ignored.
+      attribute :context_info_enabled, :equal_to => [true, false, 'true', 'false'], :default => true
+      # Specifies individual container contexts to propagate to threads. Valid values are Classloader, JNDI, Security, and WorkArea. Values are specified in a comma-separated list and are case-insensitive. All contexts are propagated by default.
+      attribute :context_info, :kind_of => String, :default => 'Classloader,JNDI,Security,WorkArea'
+      # Specifies the priority to assign to created threads.
+      attribute :thread_priority, :kind_of => Integer, :default => 5
+      # Descriptive details about the resource.
+      attribute :description, :kind_of => String, :default => ''
+      attribute :properties, :kind_of => Hash, :default => {}
+      attribute :deploymentorder, :kind_of => Fixnum, :default => 100
+
+      action :create do
+        create(resource_property_prefix)
+      end
+
+      action :destroy do
+        destroy(resource_property_prefix)
+      end
+
+      def resource_property_prefix
+        "resources.managed-thread-factory.#{self.name}."
+      end
+
+      def properties_to_record_in_create
+        {'object-type' => 'user', 'jndi-name' => self.name, 'deployment-order' => '100'}
+      end
+
+      def properties_to_set_in_create
+        property_map = {}
+
+        collect_property_sets(resource_property_prefix, property_map)
+
+        property_map['description'] = self.description
+        property_map['context-info-enabled'] = self.context_info_enabled
+        property_map['context-info'] = self.context_info
+        property_map['enabled'] = self.enabled
+        property_map['thread-priority'] = self.thread_priority
+
+        property_map
+      end
+
+      def do_create
+        args = []
+
+        args << '--enabled' << self.enabled.to_s
+        args << '--contextinfoenabled' << self.context_info_enabled.to_s
+        args << '--contextinfo' << self.context_info.to_s
+        args << '--threadpriority' << self.thread_priority.to_s
+        args << '--property' << encode_parameters(self.properties) unless self.properties.empty?
+        args << '--description' << self.description.to_s
+        args << self.name.to_s
+
+        context.exec('create-managed-thread-factory', args)
+      end
+
+      def do_destroy
+        context.exec('delete-managed-thread-factory', [self.name])
+      end
+
+      def present?
+        (context.exec('list-managed-thread-factorys', [], :terse => true, :echo => false) =~ /^#{Regexp.escape(self.name)}$/)
+      end
+    end
+  end
+end
---
+++ test/tasks/test_managed_thread_factory.rb
@@ -0,0 +1,280 @@
+require File.expand_path('../../helper', __FILE__)
+
+class Redfish::Tasks::TestManagedThreadFactory < Redfish::Tasks::BaseTaskTest
+  def test_create_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-managed-thread-factorys'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns('')
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('create-managed-thread-factory'),
+                                 equals(['--enabled', 'true', '--contextinfoenabled', 'true', '--contextinfo', 'Classloader,JNDI,Security', '--threadpriority', '6', '--property', 'SomeKey=SomeValue', '--description', 'Blah blah', 'MyThreadFactory']),
+                                 equals({})).
+      returns('')
+    executor.expects(:exec).with(equals(t.context), equals('get'),
+                                 equals(["#{property_prefix}deployment-order"]),
+                                 equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}deployment-order=100\n")
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-managed-thread-factorys'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns("MyThreadFactory\n")
+    executor.expects(:exec).with(equals(t.context), equals('get'), equals(%W(#{property_prefix}property.*)), equals(:terse => true, :echo => false)).
+      returns('')
+
+    expected_local_properties.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present_but_modified
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-managed-thread-factorys'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns("MyThreadFactory\n")
+    # Return a property that should be deleted
+    executor.expects(:exec).with(equals(t.context), equals('get'), equals(%W(#{property_prefix}property.*)), equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}property.Blah=Y\n")
+
+    values = expected_local_properties
+    values['deployment-order'] = '101'
+    values['enabled'] = 'false'
+
+    values.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}enabled=true"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}deployment-order=100"]),
+                                 equals(:terse => true, :echo => false))
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('get'),
+                                 equals(["#{property_prefix}property.Blah"]),
+                                 equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}property.Blah=X\n")
+
+    # This is the set to remove property that should not exist
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}property.Blah="]),
+                                 equals(:terse => true, :echo => false)).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties({})
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('create-managed-thread-factory'),
+                                 equals(['--enabled', 'true', '--contextinfoenabled', 'true', '--contextinfo', 'Classloader,JNDI,Security', '--threadpriority', '6', '--property', 'SomeKey=SomeValue', '--description', 'Blah blah', 'MyThreadFactory']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present_but_modified
+    cache_values = expected_properties
+
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    cache_values["#{property_prefix}enabled"] = 'false'
+    cache_values["#{property_prefix}description"] = 'XXX'
+    cache_values["#{property_prefix}deployment-order"] = '101'
+
+    # This property should be removed
+    cache_values["#{property_prefix}property.Blah"] = 'X'
+
+    t.context.cache_properties(cache_values)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}property.Blah="]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}deployment-order=100"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}description=Blah blah"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}enabled=true"]),
+                                 equals(:terse => true, :echo => false))
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present
+    t = new_task
+
+    t.context.cache_properties(expected_properties)
+
+    t.options = resource_parameters
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'name' => 'MyThreadFactory'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-managed-thread-factorys'),
+                                 equals([]),
+                                 equals({:terse => true, :echo => false})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'name' => 'MyThreadFactory'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-managed-thread-factorys'),
+                                 equals([]),
+                                 equals({:terse => true, :echo => false})).
+      returns("MyThreadFactory\n")
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('delete-managed-thread-factory'),
+                                 equals(['MyThreadFactory']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_not_present
+    t = new_task
+
+    t.context.cache_properties({})
+    t.options = {'name' => 'MyThreadFactory'}
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties(expected_properties)
+    t.options = {'name' => 'MyThreadFactory'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('delete-managed-thread-factory'),
+                                 equals(['MyThreadFactory']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  protected
+
+  def property_prefix
+    'resources.managed-thread-factory.MyThreadFactory.'
+  end
+
+  # Properties in GlassFish properties directory
+  def expected_local_properties
+    {
+      'description' => 'Blah blah',
+      'enabled' => 'true',
+      'context-info-enabled' => 'true',
+      'context-info' => 'Classloader,JNDI,Security',
+      'property.SomeKey' => 'SomeValue',
+      'thread-priority' => 6,
+      'deployment-order' => '100'
+    }
+  end
+
+  # Resource parameters
+  def resource_parameters
+    {
+      'name' => 'MyThreadFactory',
+      'enabled' => 'true',
+      'thread_priority' => 6,
+      'context_info_enabled' => 'true',
+      'context_info' => 'Classloader,JNDI,Security',
+      'description' => 'Blah blah',
+      'properties' => {'SomeKey' => 'SomeValue'},
+      'deploymentorder' => 100
+    }
+  end
+end
