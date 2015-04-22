e7162e3ae6c3cb8cc71182c0f5e41ab57e315041
Peter Donald <peter@realityforge.org>
2015-04-22 11:47:34 +1000

    Add the connector_connection_pool task/resource

---
+++ lib/redfish/tasks/connector_connection_pool.rb
@@ -0,0 +1,150 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Redfish
+  module Tasks
+    class ConnectorConnectionPool < BaseResourceTask
+
+      private
+
+      ConnectorAttribute = Struct.new('ConnectorAttribute', :key, :type, :cmdline_arg, :property_key, :default_value, :options)
+
+      ATTRIBUTES = []
+
+      def self.cmdline_arg(key, options)
+        options.delete(:cmdline_arg) || key.to_s.gsub('_','')
+      end
+
+      def self.at(key, type, property_key, default_value, options)
+        opt = options.dup
+        ATTRIBUTES << ConnectorAttribute.new(key, type, cmdline_arg(key, opt), property_key, default_value, opt)
+      end
+
+      def self.str(key, property_key, default_value = '', options = {})
+        at(key, :string, property_key, default_value, options)
+      end
+
+      def self.num(key, property_key, default_value = 0, options = {})
+        at(key, :numeric, property_key, default_value, options)
+      end
+
+      def self.bool(key, property_key, default_value = true, options = {})
+        at(key, :boolean, property_key, default_value, options)
+      end
+
+      def self.opt(key, property_key, values, default_value, options = {})
+        at(key, values, property_key, default_value, options)
+      end
+
+      str(:resource_adapter_name, 'resource-adapter-name', nil, :required => true, :cmdline_arg => 'raname')
+      str(:connection_definition_name, 'connection-definition-name', nil, :required => true, :cmdline_arg => 'connectiondefinition')
+
+      num(:steady_pool_size, 'steady-pool-size', 1)
+      num(:max_pool_size, 'max-pool-size', 250)
+      num(:max_wait, 'max-wait-time-in-millis', 60000)
+      num(:pool_resize, 'pool-resize-quantity', 2)
+      num(:idle_timeout, 'idle-timeout-in-seconds', 300)
+      num(:leak_timeout, 'connection-leak-timeout-in-seconds')
+      num(:validate_at_most_once_period_in_seconds, 'validate-atmost-once-period-in-seconds', 0, :cmdline_arg => 'validateatmostonceperiod')
+      num(:max_connection_usage_count, 'max-connection-usage-count')
+      num(:creation_retry_attempts, 'connection-creation-retry-attempts')
+      num(:creation_retry_interval, 'connection-creation-retry-interval-in-seconds', 10)
+
+      bool(:is_connection_validation_required, 'is-connection-validation-required', true, :cmdline_arg => 'isconnectvalidatereq')
+      bool(:fail_connection, 'fail-all-connections', false)
+      bool(:leak_reclaim, 'connection-leak-reclaim', false)
+      bool(:lazy_connection_enlistment, 'lazy-connection-enlistment', false)
+      bool(:lazy_connection_association, 'lazy-connection-association', false)
+      bool(:associate_with_thread, 'associate-with-thread', false)
+      bool(:match_connections, 'match-connections')
+      bool(:ping, 'ping')
+      bool(:pooling, 'pooling')
+
+      opt(:transaction_support,
+          'transaction-support',
+          %w(XATransaction LocalTransaction NoTransaction),
+          nil)
+
+      public
+
+      attribute :name, :kind_of => String, :required => true
+      attribute :description, :kind_of => String, :default => ''
+      attribute :properties, :kind_of => Hash, :default => {}
+      attribute :deployment_order, :kind_of => Fixnum, :default => 100
+
+      ATTRIBUTES.each do |attr|
+        if attr.type == :string
+          attribute attr.key, attr.options.merge(:kind_of => String, :default => attr.default_value)
+        elsif attr.type == :numeric
+          attribute attr.key, attr.options.merge(:kind_of => [Fixnum, String], :regex => /^[0-9]+$/, :default => attr.default_value)
+        elsif attr.type == :boolean
+          attribute attr.key, attr.options.merge(:equal_to => [true, false, 'true', 'false'], :default => attr.default_value)
+        elsif attr.type.is_a?(Array)
+          attribute attr.key, attr.options.merge(:equal_to => attr.type, :default => attr.default_value)
+        end
+      end
+
+      action :create do
+        create(resource_property_prefix)
+      end
+
+      action :destroy do
+        destroy(resource_property_prefix)
+      end
+
+      def resource_property_prefix
+        "resources.connector-connection-pool.#{self.name}."
+      end
+
+      def properties_to_record_in_create
+        {'object-type' => 'user', 'name' => self.name, 'deployment-order' => '100'}
+      end
+
+      def properties_to_set_in_create
+        property_map = {}
+        collect_property_sets(resource_property_prefix, property_map)
+
+        ATTRIBUTES.each do |attr|
+          property_map[attr.property_key] = self.send(attr.key)
+        end
+        property_map['description'] = self.description
+        property_map
+      end
+
+      def do_create
+        args = []
+        ATTRIBUTES.each do |attr|
+          args << "--#{attr.cmdline_arg}=#{self.send(attr.key)}"
+        end
+
+        args << '--property' << encode_parameters(self.properties) unless self.properties.empty?
+        args << '--description' << self.description
+        args << self.name
+
+        context.exec('create-connector-connection-pool', args)
+      end
+
+      def do_destroy
+        args = []
+        args << '--cascade=true'
+        args << self.name
+        context.exec('delete-connector-connection-pool', args)
+      end
+
+      def present?
+        (context.exec('list-connector-connection-pools', [], :terse => true, :echo => false) =~ /^#{Regexp.escape(self.name)}$/)
+      end
+    end
+  end
+end
---
+++ test/tasks/test_connector_connection_pool.rb
@@ -0,0 +1,332 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path('../../helper', __FILE__)
+
+class Redfish::Tasks::TestConnectorConnectionPool < Redfish::Tasks::BaseTaskTest
+  def test_create_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-connector-connection-pools'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns('')
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('create-connector-connection-pool'),
+                                 equals(['--raname=jmsra', '--connectiondefinition=javax.jms.QueueConnectionFactory', '--steadypoolsize=1', '--maxpoolsize=250', '--maxwait=60000', '--poolresize=2', '--idletimeout=300', '--leaktimeout=0', '--validateatmostonceperiod=0', '--maxconnectionusagecount=0', '--creationretryattempts=0', '--creationretryinterval=10', '--isconnectvalidatereq=true', '--failconnection=false', '--leakreclaim=false', '--lazyconnectionenlistment=false', '--lazyconnectionassociation=false', '--associatewiththread=false', '--matchconnections=true', '--ping=true', '--pooling=true', '--transactionsupport=NoTransaction', '--property', 'User=sa:Password=password', '--description', 'Audit Connection Pool', 'MyConnectorConnectionPool']),
+                                 equals({})).
+      returns('')
+    executor.expects(:exec).with(equals(t.context), equals('get'),
+                                 equals(["#{property_prefix}deployment-order"]),
+                                 equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}deployment-order=100\n")
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-connector-connection-pools'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns("MyConnectorConnectionPool\n")
+    executor.expects(:exec).with(equals(t.context), equals('get'), equals(%W(#{property_prefix}property.*)), equals(:terse => true, :echo => false)).
+      returns('')
+
+    expected_local_properties.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present_but_modified
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-connector-connection-pools'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns("MyConnectorConnectionPool\n")
+    # Return a property that should be deleted
+    executor.expects(:exec).with(equals(t.context), equals('get'), equals(%W(#{property_prefix}property.*)), equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}property.DatabaseName2=MYDB\n")
+
+    cache_values = expected_local_properties
+    cache_values['description'] = 'X'
+    cache_values['ping'] = 'false'
+    cache_values['deployment-order'] = '99'
+    cache_values['property.Password'] = 'secret'
+    cache_values['property.DatabaseName2'] = 'MYDB'
+
+    cache_values.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}property.Password=password"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}deployment-order=100"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}description=Audit Connection Pool"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}ping=true"]),
+                                 equals(:terse => true, :echo => false))
+
+    # This is the set to remove property that should not exist
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}property.DatabaseName2="]),
+                                 equals(:terse => true, :echo => false)).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties({})
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('create-connector-connection-pool'),
+                                 equals(['--raname=jmsra', '--connectiondefinition=javax.jms.QueueConnectionFactory', '--steadypoolsize=1', '--maxpoolsize=250', '--maxwait=60000', '--poolresize=2', '--idletimeout=300', '--leaktimeout=0', '--validateatmostonceperiod=0', '--maxconnectionusagecount=0', '--creationretryattempts=0', '--creationretryinterval=10', '--isconnectvalidatereq=true', '--failconnection=false', '--leakreclaim=false', '--lazyconnectionenlistment=false', '--lazyconnectionassociation=false', '--associatewiththread=false', '--matchconnections=true', '--ping=true', '--pooling=true', '--transactionsupport=NoTransaction', '--property', 'User=sa:Password=password', '--description', 'Audit Connection Pool', 'MyConnectorConnectionPool']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present_but_modified
+    cache_values = expected_properties
+
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    cache_values["#{property_prefix}ping"] = 'false'
+    cache_values["#{property_prefix}description"] = 'XXX'
+    cache_values["#{property_prefix}deployment-order"] = '101'
+    cache_values["#{property_prefix}property.Password"] = 'secret'
+
+    # This property should be removed
+    cache_values["#{property_prefix}property.DatabaseName2"] = 'MYDB'
+
+    t.context.cache_properties(cache_values)
+
+    t.options = resource_parameters
+
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}property.Password=password"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}deployment-order=100"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}description=Audit Connection Pool"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}ping=true"]),
+                                 equals(:terse => true, :echo => false))
+
+    # This is the set to remove property that should not exist
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}property.DatabaseName2="]),
+                                 equals(:terse => true, :echo => false)).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present
+    t = new_task
+
+    t.context.cache_properties(expected_properties)
+
+    t.options = resource_parameters
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'name' => 'MyConnectorConnectionPool'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-connector-connection-pools'),
+                                 equals([]),
+                                 equals({:terse => true, :echo => false})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'name' => 'MyConnectorConnectionPool'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-connector-connection-pools'),
+                                 equals([]),
+                                 equals({:terse => true, :echo => false})).
+      returns("MyConnectorConnectionPool\n")
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('delete-connector-connection-pool'),
+                                 equals(['--cascade=true', 'MyConnectorConnectionPool']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_not_present
+    t = new_task
+
+    t.context.cache_properties({})
+    t.options = {'name' => 'MyConnectorConnectionPool'}
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties(expected_properties)
+    t.options = {'name' => 'MyConnectorConnectionPool'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('delete-connector-connection-pool'),
+                                 equals(['--cascade=true', 'MyConnectorConnectionPool']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  protected
+
+  def property_prefix
+    'resources.connector-connection-pool.MyConnectorConnectionPool.'
+  end
+
+  # Properties in GlassFish properties directory
+  def expected_local_properties
+    {
+      'connection-definition-name' => 'javax.jms.QueueConnectionFactory',
+      'resource-adapter-name' => 'jmsra',
+      'transaction-support' => 'NoTransaction',
+      'description' => 'Audit Connection Pool',
+      'steady-pool-size' => '1',
+      'max-pool-size' => '250',
+      'max-wait-time-in-millis' => '60000',
+      'pool-resize-quantity' => '2',
+      'idle-timeout-in-seconds' => '300',
+      'validate-atmost-once-period-in-seconds' => '0',
+      'connection-leak-timeout-in-seconds' => '0',
+      'connection-creation-retry-attempts' => '0',
+      'connection-creation-retry-interval-in-seconds' => '10',
+      'max-connection-usage-count' => '0',
+      'is-connection-validation-required' => 'true',
+      'fail-all-connections' => 'false',
+      'connection-leak-reclaim' => 'false',
+      'lazy-connection-enlistment' => 'false',
+      'lazy-connection-association' => 'false',
+      'associate-with-thread' => 'false',
+      'match-connections' => 'true',
+      'ping' => 'true',
+      'deployment-order' => '100',
+      'pooling' => 'true',
+      'property.User' => 'sa',
+      'property.Password' => 'password'
+    }
+  end
+
+  # Resource parameters
+  def resource_parameters
+    {
+      'name' => 'MyConnectorConnectionPool',
+      'resource_adapter_name' => 'jmsra',
+      'connection_definition_name' => 'javax.jms.QueueConnectionFactory',
+      'transaction_support' => 'NoTransaction',
+      'description' => 'Audit Connection Pool',
+      'is_connection_validation_required' => 'true',
+      'ping' => 'true',
+      'deployment_order' => 100,
+      'properties' =>
+        {
+          'User' => 'sa',
+          'Password' => 'password'
+        }
+    }
+  end
+end
