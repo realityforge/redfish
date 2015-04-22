090f5958832a3f9df9c25758ce23334adee44d35
Peter Donald <peter@realityforge.org>
2015-04-22 10:07:49 +1000

    Add ensure action to property so we can ensure a property is a particular value without trying to change it

--- lib/redfish/tasks/property.rb
+++ lib/redfish/tasks/property.rb
@@ -8,15 +8,27 @@ module Redfish
       private
 
       action :set do
+        when_value_not_set do
+          context.exec('set', ["#{self.key}=#{self.value}"], :terse => true, :echo => false)
+          updated_by_last_action
+          if context.property_cache?
+            context.property_cache[self.key] = self.value
+          end
+        end
+      end
+
+      action :ensure do
+        when_value_not_set do
+          raise "Property '#{self.key}' expected to be '#{self.value}'"
+        end
+      end
+
+      def when_value_not_set
         cache_present = context.property_cache?
         may_need_update = cache_present ? self.value != context.property_cache[self.key] : true
         if may_need_update
           if cache_present || !(/^#{Regexp.escape("#{self.key}=#{self.value}")}$/ =~ context.exec('get', ["#{self.key}"], :terse => true, :echo => false))
-            context.exec('set', ["#{self.key}=#{self.value}"], :terse => true, :echo => false)
-            updated_by_last_action
-            if cache_present
-              context.property_cache[self.key] = self.value
-            end
+            yield if block_given?
           end
         end
       end
--- test/tasks/test_property.rb
+++ test/tasks/test_property.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../helper', __FILE__)
 
 class Redfish::Tasks::TestProperty < Redfish::Tasks::BaseTaskTest
-  def test_property_no_cache_and_not_set
+  def test_set_property_no_cache_and_not_set
     executor = Redfish::Executor.new
     t = new_task(executor)
 
@@ -15,7 +15,7 @@ class Redfish::Tasks::TestProperty < Redfish::Tasks::BaseTaskTest
     ensure_task_updated_by_last_action(t)
   end
 
-  def test_property_no_cache_and_already_set
+  def test_set_property_no_cache_and_already_set
     executor = Redfish::Executor.new
     t = new_task(executor)
 
@@ -28,7 +28,7 @@ class Redfish::Tasks::TestProperty < Redfish::Tasks::BaseTaskTest
     ensure_task_not_updated_by_last_action(t)
   end
 
-  def test_property_cache_and_not_set
+  def test_set_property_cache_and_not_set
     executor = Redfish::Executor.new
     t = new_task(executor)
 
@@ -44,7 +44,7 @@ class Redfish::Tasks::TestProperty < Redfish::Tasks::BaseTaskTest
     assert_equal t.context.property_cache['configs.config.server-config.security-service.activate-default-principal-to-role-mapping'], 'true'
   end
 
-  def test_property_cache_and_set
+  def test_set_property_cache_and_set
     t = new_task
 
     t.context.cache_properties('configs.config.server-config.security-service.activate-default-principal-to-role-mapping' => 'true')
@@ -52,6 +52,91 @@ class Redfish::Tasks::TestProperty < Redfish::Tasks::BaseTaskTest
     t.key = 'configs.config.server-config.security-service.activate-default-principal-to-role-mapping'
     t.value = 'true'
     t.perform_action(:set)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_ensure_when_value_not_matches
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('get'),
+                                 equals(%w(configs.config.server-config.security-service.activate-default-principal-to-role-mapping)),
+                                 equals(:terse => true, :echo => false)).
+      returns("configs.config.server-config.security-service.activate-default-principal-to-role-mapping=false\n")
+
+    t.key = 'configs.config.server-config.security-service.activate-default-principal-to-role-mapping'
+    t.value = 'true'
+
+    matched = true
+    begin
+      t.perform_action(:ensure)
+    rescue => e
+      assert_equal e.message, "Property 'configs.config.server-config.security-service.activate-default-principal-to-role-mapping' expected to be 'true'"
+      matched = false
+    end
+
+    fail('Expected value to be different') if matched
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_ensure_when_value_matches
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('get'),
+                                 equals(%w(configs.config.server-config.security-service.activate-default-principal-to-role-mapping)),
+                                 equals(:terse => true, :echo => false)).
+      returns("configs.config.server-config.security-service.activate-default-principal-to-role-mapping=true\n")
+
+    t.key = 'configs.config.server-config.security-service.activate-default-principal-to-role-mapping'
+    t.value = 'true'
+    begin
+      t.perform_action(:ensure)
+    rescue
+      fail('Expected value to be the same')
+    end
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_ensure_when_cached_value_matches
+    t = new_task
+
+    t.context.cache_properties('configs.config.server-config.security-service.activate-default-principal-to-role-mapping' => 'true')
+
+    t.key = 'configs.config.server-config.security-service.activate-default-principal-to-role-mapping'
+    t.value = 'true'
+    begin
+      t.perform_action(:ensure)
+    rescue
+      fail('Expected value to be the same')
+    end
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_ensure_when_cached_value_not_matches
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties('configs.config.server-config.security-service.activate-default-principal-to-role-mapping' => 'false')
+
+    t.key = 'configs.config.server-config.security-service.activate-default-principal-to-role-mapping'
+    t.value = 'true'
+
+    matched = true
+    begin
+      t.perform_action(:ensure)
+    rescue => e
+      assert_equal e.message, "Property 'configs.config.server-config.security-service.activate-default-principal-to-role-mapping' expected to be 'true'"
+      matched = false
+    end
+
+    fail('Expected value to be different') if matched
 
     ensure_task_not_updated_by_last_action(t)
   end
