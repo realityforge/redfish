ddb717bed3baeb713602b9987da1be11b3b170fb
Peter Donald <peter@realityforge.org>
2015-04-22 13:03:07 +1000

    Define connector resource

---
+++ lib/redfish/tasks/connector_resource.rb
@@ -0,0 +1,80 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Redfish
+  module Tasks
+    class ConnectorResource < BaseResourceTask
+
+      attribute :name, :kind_of => String, :required => true
+      attribute :connector_pool_name, :kind_of => String, :required => true
+      attribute :object_type, :equal_to => %w(user system-instance system-admin system-all), :default => 'user'
+
+      attribute :enabled, :equal_to => [true, false, 'true', 'false'], :default => true
+      attribute :description, :kind_of => String, :default => ''
+      attribute :properties, :kind_of => Hash, :default => {}
+      attribute :deployment_order, :kind_of => Fixnum, :default => 100
+
+      action :create do
+        create(resource_property_prefix)
+      end
+
+      action :destroy do
+        destroy(resource_property_prefix)
+      end
+
+      def immutable_local_properties
+        ['object-type']
+      end
+
+      def resource_property_prefix
+        "resources.connector-resource.#{self.name}."
+      end
+
+      def properties_to_record_in_create
+        {'object-type' => self.object_type, 'jndi-name' => self.name, 'deployment-order' => '100'}
+      end
+
+      def properties_to_set_in_create
+        property_map = {'description' => self.description}
+
+        collect_property_sets(resource_property_prefix, property_map)
+
+        property_map['enabled'] = self.enabled
+        property_map['pool-name'] = self.connector_pool_name
+
+        property_map
+      end
+
+      def do_create
+        args = []
+        args << '--enabled' << self.enabled.to_s
+        args << '--objecttype' << self.object_type.to_s
+        args << '--poolname' << self.connector_pool_name.to_s
+        args << '--property' << encode_parameters(self.properties) unless self.properties.empty?
+        args << '--description' << self.description.to_s
+        args << self.name.to_s
+
+        context.exec('create-connector-resource', args)
+      end
+
+      def do_destroy
+        context.exec('delete-connector-resource', [self.name])
+      end
+
+      def present?
+        (context.exec('list-connector-resources', [], :terse => true, :echo => false) =~ /^#{Regexp.escape(self.name)}$/)
+      end
+    end
+  end
+end
---
+++ test/tasks/test_connector_resource.rb
@@ -0,0 +1,288 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path('../../helper', __FILE__)
+
+class Redfish::Tasks::TestConnectorResource < Redfish::Tasks::BaseTaskTest
+  def test_create_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-connector-resources'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns('')
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('create-connector-resource'),
+                                 equals(['--enabled', 'true', '--objecttype', 'user', '--poolname', 'MyConnectorPool', '--description', 'Connector Blah', 'MyConnectorResource']),
+                                 equals({})).
+      returns('')
+    executor.expects(:exec).with(equals(t.context), equals('get'),
+                                 equals(["#{property_prefix}deployment-order"]),
+                                 equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}deployment-order=100\n")
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-connector-resources'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns("MyConnectorResource\n")
+    executor.expects(:exec).with(equals(t.context), equals('get'), equals(%W(#{property_prefix}property.*)), equals(:terse => true, :echo => false)).
+      returns('')
+
+    expected_local_properties.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present_but_modified
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-connector-resources'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns("MyConnectorResource\n")
+    # Return a property that should be deleted
+    executor.expects(:exec).with(equals(t.context), equals('get'), equals(%W(#{property_prefix}property.*)), equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}property.Blah=Y\n")
+
+    values = expected_local_properties
+    values['deployment-order'] = '101'
+    values['enabled'] = 'false'
+
+    values.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}enabled=true"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}deployment-order=100"]),
+                                 equals(:terse => true, :echo => false))
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('get'),
+                                 equals(["#{property_prefix}property.Blah"]),
+                                 equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}property.Blah=X\n")
+
+    # This is the set to remove property that should not exist
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}property.Blah="]),
+                                 equals(:terse => true, :echo => false)).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties({})
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('create-connector-resource'),
+                                 equals(['--enabled', 'true', '--objecttype', 'user', '--poolname', 'MyConnectorPool', '--description', 'Connector Blah', 'MyConnectorResource']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present_but_modified
+    cache_values = expected_properties
+
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    cache_values["#{property_prefix}enabled"] = 'false'
+    cache_values["#{property_prefix}description"] = 'XXX'
+    cache_values["#{property_prefix}deployment-order"] = '101'
+
+    # This property should be removed
+    cache_values["#{property_prefix}property.Blah"] = 'X'
+
+    t.context.cache_properties(cache_values)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}property.Blah="]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}deployment-order=100"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}description=Connector Blah"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}enabled=true"]),
+                                 equals(:terse => true, :echo => false))
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present
+    t = new_task
+
+    t.context.cache_properties(expected_properties)
+
+    t.options = resource_parameters
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'name' => 'MyConnectorResource'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-connector-resources'),
+                                 equals([]),
+                                 equals({:terse => true, :echo => false})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'name' => 'MyConnectorResource'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-connector-resources'),
+                                 equals([]),
+                                 equals({:terse => true, :echo => false})).
+      returns("MyConnectorResource\n")
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('delete-connector-resource'),
+                                 equals(['MyConnectorResource']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_not_present
+    t = new_task
+
+    t.context.cache_properties({})
+    t.options = {'name' => 'MyConnectorResource'}
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties(expected_properties)
+    t.options = {'name' => 'MyConnectorResource'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('delete-connector-resource'),
+                                 equals(['MyConnectorResource']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  protected
+
+  def property_prefix
+    'resources.connector-resource.MyConnectorResource.'
+  end
+
+  # Properties in GlassFish properties directory
+  def expected_local_properties
+    {
+      'description' => 'Connector Blah',
+      'enabled' => 'true',
+      'pool-name' => 'MyConnectorPool',
+      'deployment-order' => '100'
+    }
+  end
+
+  # Resource parameters
+  def resource_parameters
+    {
+      'name' => 'MyConnectorResource',
+      'enabled' => 'true',
+      'connector_pool_name' => 'MyConnectorPool',
+      'description' => 'Connector Blah',
+      'deployment_order' => 100
+    }
+  end
+end
