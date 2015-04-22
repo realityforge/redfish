ea7e551c425a83d151eabafd571cadc73aa5cc4f
Peter Donald <peter@realityforge.org>
2015-04-22 16:24:23 +1000

    Add in a library resource

---
+++ lib/redfish/tasks/library.rb
@@ -0,0 +1,48 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Redfish
+  module Tasks
+    class Library < AsadminTask
+      attribute :file, :kind_of => String, :required => true
+      attribute :library_type, :equal_to => %w(common ext app), :default => 'common'
+      attribute :upload, :equal_to => [true, false, 'true', 'false'], :default => false
+
+      action :create do
+        unless present?
+          args = []
+          args << '--type' << self.library_type.to_s
+          args << '--upload' << self.upload.to_s
+          args << self.file.to_s
+
+          context.exec('add-library', args)
+
+          updated_by_last_action
+        end
+      end
+
+      action :destroy do
+        if present?
+          context.exec('remove-library', ['--type', self.library_type.to_s, File.basename(self.file)])
+
+          updated_by_last_action
+        end
+      end
+
+      def present?
+        (context.exec('list-libraries', ['--type', self.library_type.to_s], :terse => true, :echo => false) =~ /^#{Regexp.escape(File.basename(self.file))}$/)
+      end
+    end
+  end
+end
---
+++ test/tasks/test_library.rb
@@ -0,0 +1,96 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path('../../helper', __FILE__)
+
+class Redfish::Tasks::TestLibrary < Redfish::Tasks::BaseTaskTest
+  def test_create_when_no_such_library
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-libraries'),
+                                 equals(%w(--type ext)),
+                                 equals(:terse => true, :echo => false)).
+      returns('')
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('add-library'),
+                                 equals(%w(--type ext --upload false /opt/jtds/jtds-1.3.1.jar)),
+                                 equals({})).
+      returns('')
+
+    t.file = '/opt/jtds/jtds-1.3.1.jar'
+    t.library_type = 'ext'
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_when_library_exists
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-libraries'),
+                                 equals(%w(--type ext)),
+                                 equals(:terse => true, :echo => false)).
+      returns("jtds-1.3.1.jar\n")
+
+    t.file = '/opt/jtds/jtds-1.3.1.jar'
+    t.library_type = 'ext'
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_destroy_when_library_exists
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-libraries'),
+                                 equals(%w(--type ext)),
+                                 equals(:terse => true, :echo => false)).
+      returns("jtds-1.3.1.jar\n")
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('remove-library'),
+                                 equals(%w(--type ext jtds-1.3.1.jar)),
+                                 equals({})).
+      returns('')
+
+    t.file = '/opt/jtds/jtds-1.3.1.jar'
+    t.library_type = 'ext'
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+
+  def test_destroy_when_library_no_exist
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-libraries'),
+                                 equals(%w(--type ext)),
+                                 equals(:terse => true, :echo => false)).
+      returns('')
+
+    t.file = '/opt/jtds/jtds-1.3.1.jar'
+    t.library_type = 'ext'
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+end
