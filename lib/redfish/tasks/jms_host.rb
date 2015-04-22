47a7c43974d11dcd61964d93ff943c6b24038ed7
Peter Donald <peter@realityforge.org>
2015-04-22 21:38:59 +1000

    Add jms_host resource

---
+++ lib/redfish/tasks/jms_host.rb
@@ -0,0 +1,76 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Redfish
+  module Tasks
+    class JmsHost < BaseResourceTask
+
+      attribute :name, :kind_of => String, :required => true
+      # The host name for the JMS service.
+      attribute :host, :kind_of => String, :required => true
+      # The port number used by the JMS service.
+      attribute :port, :kind_of => Integer, :required => true
+      # The user name for the JMS service.
+      attribute :admin_username, :kind_of => String, :required => true
+      # The password for the JMS service.
+      attribute :admin_password, :kind_of => String, :required => true
+
+      action :create do
+        create(resource_property_prefix)
+      end
+
+      action :destroy do
+        destroy(resource_property_prefix)
+      end
+
+      def resource_property_prefix
+        "configs.config.server-config.jms-service.jms-host.#{self.name}."
+      end
+
+      def properties_to_record_in_create
+        {'lazy-init' => 'true', 'name' => self.name}
+      end
+
+      def properties_to_set_in_create
+        property_map = {}
+
+        property_map['admin-user'] = self.admin_username
+        property_map['admin-password'] = self.admin_password
+        property_map['host'] = self.host
+        property_map['port'] = self.port
+        property_map
+      end
+
+      def do_create
+        args = []
+
+        args << '--mqhost' << self.host.to_s
+        args << '--mqport' << self.port.to_s
+        args << '--mquser' << self.admin_username.to_s
+        args << '--mqpassword' << self.admin_password.to_s
+        args << self.name.to_s
+
+        context.exec('create-jms-host', args)
+      end
+
+      def do_destroy
+        context.exec('delete-jms-host', [self.name])
+      end
+
+      def present?
+        (context.exec('list-jms-hosts', [], :terse => true, :echo => false) =~ /^#{Regexp.escape(self.name)}$/)
+      end
+    end
+  end
+end
---
+++ test/tasks/test_jms_host.rb
@@ -0,0 +1,244 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path('../../helper', __FILE__)
+
+class Redfish::Tasks::TestJmsHost < Redfish::Tasks::BaseTaskTest
+  def test_create_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-jms-hosts'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns('')
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('create-jms-host'),
+                                 equals(['--mqhost', 'mq.example.com', '--mqport', '99', '--mquser', 'god', '--mqpassword', 'secret', 'MyJmsHost']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-jms-hosts'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns("MyJmsHost\n")
+
+    expected_local_properties.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present_but_modified
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-jms-hosts'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns("MyJmsHost\n")
+
+    values = expected_local_properties
+    values['port'] = '101'
+
+    values.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}port=99"]),
+                                 equals(:terse => true, :echo => false))
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties({})
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('create-jms-host'),
+                                 equals(['--mqhost', 'mq.example.com', '--mqport', '99', '--mquser', 'god', '--mqpassword', 'secret', 'MyJmsHost']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present_but_modified
+    cache_values = expected_properties
+
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    cache_values["#{property_prefix}port"] = '101'
+
+    t.context.cache_properties(cache_values)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}port=99"]),
+                                 equals(:terse => true, :echo => false))
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present
+    t = new_task
+
+    t.context.cache_properties(expected_properties)
+
+    t.options = resource_parameters
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'name' => 'MyJmsHost'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-jms-hosts'),
+                                 equals([]),
+                                 equals({:terse => true, :echo => false})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'name' => 'MyJmsHost'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-jms-hosts'),
+                                 equals([]),
+                                 equals({:terse => true, :echo => false})).
+      returns("MyJmsHost\n")
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('delete-jms-host'),
+                                 equals(['MyJmsHost']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_not_present
+    t = new_task
+
+    t.context.cache_properties({})
+    t.options = {'name' => 'MyJmsHost'}
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties(expected_properties)
+    t.options = {'name' => 'MyJmsHost'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('delete-jms-host'),
+                                 equals(['MyJmsHost']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  protected
+
+  def property_prefix
+    'configs.config.server-config.jms-service.jms-host.MyJmsHost.'
+  end
+
+  # Properties in GlassFish properties directory
+  def expected_local_properties
+    {
+      'host' => 'mq.example.com',
+      'port' => '99',
+      'admin-user' => 'god',
+      'admin-password' => 'secret'
+    }
+  end
+
+  # Resource parameters
+  def resource_parameters
+    {
+      'name' => 'MyJmsHost',
+      'host' => 'mq.example.com',
+      'port' => 99,
+      'admin_username' => 'god',
+      'admin_password' => 'secret'
+    }
+  end
+end
