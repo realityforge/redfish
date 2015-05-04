a10f3a294982436fda3efd45149e3fa62ff71b93
Peter Donald <peter@realityforge.org>
2015-05-04 16:30:52 +1000

    Initial work towards implementing an application task

---
+++ lib/redfish/tasks/application.rb
@@ -0,0 +1,134 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Redfish
+  module Tasks
+    class Application < BaseResourceTask
+
+      TYPES = %w(car ear ejb osgi rar war)
+
+      attribute :name, :kind_of => String, :required => true
+      attribute :location, :kind_of => String, :default => nil
+      attribute :deployment_plan, :kind_of => String, :default => nil
+      attribute :context_root, :kind_of => String, :default => nil
+      attribute :virtual_servers, :kind_of => Array, :default => []
+      attribute :enabled, :equal_to => [true, false, 'true', 'false'], :default => true
+      attribute :type, :equal_to => TYPES + TYPES.collect { |a| a.to_sym } + [nil], :default => nil
+
+      attribute :generate_rmi_stubs, :equal_to => [true, false, 'true', 'false'], :default => false
+      attribute :availability_enabled, :equal_to => [true, false, 'true', 'false'], :default => false
+      attribute :lb_enabled, :equal_to => [true, false, 'true', 'false'], :default => true
+      attribute :keep_state, :equal_to => [true, false, 'true', 'false'], :default => false
+      attribute :verify, :equal_to => [true, false, 'true', 'false'], :default => false
+      attribute :precompile_jsp, :equal_to => [true, false, 'true', 'false'], :default => true
+      attribute :async_replication, :equal_to => [true, false, 'true', 'false'], :default => true
+
+      attribute :properties, :kind_of => Hash, :default => {}
+      attribute :deployment_order, :kind_of => Fixnum, :default => 100
+
+      action :create do
+        @path = nil
+        create(resource_property_prefix)
+      end
+
+      action :destroy do
+        destroy(resource_property_prefix)
+      end
+
+      def resource_property_prefix
+        "applications.application.#{self.name}."
+      end
+
+      def properties_to_record_in_create
+        {'object-type' => 'user', 'name' => self.name, 'deployment-order' => '100'}
+      end
+
+      def properties_to_set_in_create
+        property_map = {}
+
+        collect_property_sets(resource_property_prefix, property_map)
+
+        property_map['enabled'] = self.enabled
+        property_map['async-replication'] = self.async_replication
+        property_map['availability-enabled'] = self.availability_enabled
+        property_map['directory-deployed'] = is_location_a_directory?
+        property_map['context-root'] = self.context_root.nil? ? '' : self.context_root
+        property_map['location'] = is_location_a_directory? ? "file:#{self.resolved_location.gsub(/\/$/, '')}/" : "${com.sun.aas.instanceRootURI}/applications/#{self.name}/"
+
+        property_map['property.defaultAppName'] = self.name
+        property_map['property.archiveType'] = self.derive_archive_type
+        property_map['property.appLocation'] = is_location_a_directory? ? "file:#{self.resolved_location.gsub(/\/$/, '')}/" : "${com.sun.aas.instanceRootURI}/applications/__internal/#{self.name}/#{File.basename(self.location)}"
+
+        property_map
+      end
+
+      def is_location_a_directory?
+        File.directory?(self.resolved_location)
+      end
+
+      def derive_archive_type
+        return self.type unless type.nil?
+        unless is_location_a_directory?
+          extension = File.extname(self.resolved_location.to_s)
+          unless extension.empty?
+            type = extension[1..-1]
+            return type if TYPES.include?(type)
+          end
+        end
+        'war'
+      end
+
+      def resolved_location
+        unless @path
+          raise 'The "location" parameter must be set when deploying application.' if self.location.nil?
+          @path = File.expand_path(self.location)
+          raise 'The "location" must reference a archive or directory that exists when deploying application.' unless File.exist?(@path)
+        end
+        @path
+      end
+
+      def do_create
+
+        args = []
+        args << '--name' << self.name.to_s
+        args << "--enabled=#{self.enabled}"
+        args << '--force=true'
+        args << '--type' << derive_archive_type
+        args << "--contextroot=#{self.context_root}" if self.context_root
+        args << "--generatermistubs=#{self.generate_rmi_stubs}"
+        args << "--availabilityenabled=#{self.availability_enabled}"
+        args << "--lbenabled=#{self.lb_enabled}"
+        args << "--keepstate=#{self.keep_state}"
+        args << "--verify=#{self.verify}"
+        args << "--precompilejsp=#{self.precompile_jsp}"
+        args << "--asyncreplication=#{self.async_replication}"
+        args << "--virtualservers=#{self.virtual_servers.join(',')}" unless self.virtual_servers.empty?
+        args << '--deploymentplan' << self.deployment_plan.to_s if self.deployment_plan
+        args << '--deploymentorder' << self.deployment_order.to_s
+        args << '--property' << encode_parameters(self.properties) unless self.properties.empty?
+        args << self.resolved_location.to_s
+
+        context.exec(is_location_a_directory? ? 'deploydir' : 'deploy', args)
+      end
+
+      def do_destroy
+        context.exec('undeploy', ["--cascade=#{self.type == 'rar'}", self.name])
+      end
+
+      def present?
+        (context.exec('list-applications', [], :terse => true, :echo => false) =~ /^#{Regexp.escape(self.name)} +\</)
+      end
+    end
+  end
+end
---
+++ test/tasks/test_application.rb
@@ -0,0 +1,322 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path('../../helper', __FILE__)
+
+class Redfish::Tasks::TestApplication < Redfish::Tasks::BaseTaskTest
+  def setup
+    super
+    @location = @deployment_plan = nil
+  end
+
+  def test_create_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-applications'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns('')
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('deploydir'),
+                                 equals(['--name', 'MyApplication', '--enabled=true', '--force=true', '--type', 'war', '--contextroot=/myapp', '--generatermistubs=true', '--availabilityenabled=true', '--lbenabled=true', '--keepstate=true', '--verify=true', '--precompilejsp=true', '--asyncreplication=true', '--deploymentplan', "#{self.temp_dir}/myapp-plan.jar", '--deploymentorder', '100', '--property', 'java-web-start-enabled=false', "#{self.temp_dir}/myapp"]),
+                                 equals({})).
+      returns('')
+    executor.expects(:exec).with(equals(t.context), equals('get'),
+                                 equals(["#{property_prefix}deployment-order"]),
+                                 equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}deployment-order=100\n")
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-applications'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns("MyApplication <web>\n")
+    executor.expects(:exec).with(equals(t.context), equals('get'), equals(%W(#{property_prefix}property.*)), equals(:terse => true, :echo => false)).
+      returns('')
+
+    expected_local_properties.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_not_present_and_element_present_but_modified
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context), equals('list-applications'), equals(%w()), equals(:terse => true, :echo => false)).
+      returns("MyApplication     <web>\n")
+    # Return a property that should be deleted
+    executor.expects(:exec).with(equals(t.context), equals('get'), equals(%W(#{property_prefix}property.*)), equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}property.Blah=Y\n")
+
+    values = expected_local_properties
+    values['deployment-order'] = '101'
+    values['enabled'] = 'false'
+
+    values.each_pair do |k, v|
+      executor.expects(:exec).with(equals(t.context),
+                                   equals('get'),
+                                   equals(["#{property_prefix}#{k}"]),
+                                   equals(:terse => true, :echo => false)).
+        returns("#{property_prefix}#{k}=#{v}\n")
+    end
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}enabled=true"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}deployment-order=100"]),
+                                 equals(:terse => true, :echo => false))
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('get'),
+                                 equals(["#{property_prefix}property.Blah"]),
+                                 equals(:terse => true, :echo => false)).
+      returns("#{property_prefix}property.Blah=X\n")
+
+    # This is the set to remove property that should not exist
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}property.Blah="]),
+                                 equals(:terse => true, :echo => false)).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties({})
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('deploydir'),
+                                 equals(['--name', 'MyApplication', '--enabled=true', '--force=true', '--type', 'war', '--contextroot=/myapp', '--generatermistubs=true', '--availabilityenabled=true', '--lbenabled=true', '--keepstate=true', '--verify=true', '--precompilejsp=true', '--asyncreplication=true', '--deploymentplan', "#{self.temp_dir}/myapp-plan.jar", '--deploymentorder', '100', '--property', 'java-web-start-enabled=false', "#{self.temp_dir}/myapp"]),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present_but_modified
+    cache_values = expected_properties
+
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    cache_values["#{property_prefix}enabled"] = 'false'
+    cache_values["#{property_prefix}deployment-order"] = '101'
+
+    # This property should be removed
+    cache_values["#{property_prefix}property.Blah"] = 'X'
+
+    t.context.cache_properties(cache_values)
+
+    t.options = resource_parameters
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}property.Blah="]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}deployment-order=100"]),
+                                 equals(:terse => true, :echo => false))
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('set'),
+                                 equals(["#{property_prefix}enabled=true"]),
+                                 equals(:terse => true, :echo => false))
+
+    t.perform_action(:create)
+
+    ensure_task_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_create_element_where_cache_present_and_element_present
+    t = new_task
+
+    t.context.cache_properties(expected_properties)
+
+    t.options = resource_parameters
+
+    t.perform_action(:create)
+
+    ensure_task_not_updated_by_last_action(t)
+
+    ensure_expected_cache_values(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_not_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'name' => 'MyApplication'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-applications'),
+                                 equals([]),
+                                 equals({:terse => true, :echo => false})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_not_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.options = {'name' => 'MyApplication'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('list-applications'),
+                                 equals([]),
+                                 equals({:terse => true, :echo => false})).
+      returns("MyApplication     <web>\n")
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('undeploy'),
+                                 equals(['--cascade=false', 'MyApplication']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_not_present
+    t = new_task
+
+    t.context.cache_properties({})
+    t.options = {'name' => 'MyApplication'}
+
+    t.perform_action(:destroy)
+
+    ensure_task_not_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  def test_delete_element_where_cache_present_and_element_present
+    executor = Redfish::Executor.new
+    t = new_task(executor)
+
+    t.context.cache_properties(expected_properties)
+    t.options = {'name' => 'MyApplication'}
+
+    executor.expects(:exec).with(equals(t.context),
+                                 equals('undeploy'),
+                                 equals(['--cascade=false', 'MyApplication']),
+                                 equals({})).
+      returns('')
+
+    t.perform_action(:destroy)
+
+    ensure_task_updated_by_last_action(t)
+    ensure_properties_not_present(t)
+  end
+
+  protected
+
+  def property_prefix
+    'applications.application.MyApplication.'
+  end
+
+  def location_as_dir
+    unless @location
+      @location = "#{self.temp_dir}/myapp"
+      FileUtils.mkdir_p @location
+      File.open("#{@location}/index.txt",'w') {|f| f << 'Hi' }
+    end
+    @location
+  end
+
+  def deployment_plan
+    unless @deployment_plan
+      @deployment_plan = "#{self.temp_dir}/myapp-plan.jar"
+      File.open(@deployment_plan,'w') {|f| f << '' }
+    end
+    @deployment_plan
+  end
+
+  # Properties in GlassFish properties directory
+  def expected_local_properties
+    {
+      'enabled' => 'true',
+      'async-replication' => 'true',
+      'availability-enabled' => 'true',
+      'directory-deployed' => 'true',
+      'context-root' => '/myapp',
+      'location' => "file:#{self.location_as_dir}/",
+      'property.defaultAppName' => 'MyApplication',
+      'property.archiveType' => 'war',
+      'property.appLocation' => "file:#{self.location_as_dir}/",
+      'property.java-web-start-enabled' => 'false',
+      'deployment-order' => '100'
+    }
+  end
+
+  # Resource parameters
+  def resource_parameters
+    {
+      'name' => 'MyApplication',
+      'location' => self.location_as_dir,
+      'deployment_plan' => self.deployment_plan,
+      'context_root' => '/myapp',
+      'enabled' => 'true',
+      'type' => 'war',
+      'generate_rmi_stubs' => 'true',
+      'availability_enabled' => 'true',
+      'lb_enabled' => 'true',
+      'keep_state' => 'true',
+      'verify' => 'true',
+      'precompile_jsp' => 'true',
+      'async_replication' => 'true',
+      'properties' => {'java-web-start-enabled' => 'false'},
+      'deployment_order' => 100
+    }
+  end
+end
